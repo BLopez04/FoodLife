0bdcb60aab40d1e1128e8301091e33f6cd1413ea
ryanvuu <ryanvu077@gmail.com>
2024-12-02 12:43:30 -0800

    Change navbar buttons to Navlink to highlight active page

--- packages/react-frontend/src/Navbar.jsx
+++ packages/react-frontend/src/Navbar.jsx
@@ -1,19 +1,34 @@
 // src/Navbar.jsx
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import classes from './Navbar.module.css';
 
 function Navbar() {
     const navigate = useNavigate();
-
     return (
         <nav className={classes.navbar}>
             <div className={classes.logo} onClick={() => navigate("/")}>FoodLife</div>
             <div className={classes.navLinks}>
-                <button className={classes.navButton} onClick={() => navigate("/")}>Home</button>
-                <button className={classes.navButton} onClick={() => navigate("/about")}>About</button>
-                <button className={classes.navButton} onClick={() => navigate("/reviews")}>Reviews</button>
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => isActive ? classes.activeNavButton : classes.navButton}
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    className={({ isActive }) => isActive ? classes.activeNavButton : classes.navButton}
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to="/reviews"
+                    className={({ isActive }) => isActive ? classes.activeNavButton : classes.navButton}
+                >
+                    Reviews
+                </NavLink>
             </div>
         </nav>
     );
