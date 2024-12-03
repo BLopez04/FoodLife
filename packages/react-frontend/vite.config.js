c57fcad66f5f9985956672a0944eb04889c4ee9b
BLopez04 <bernardo41504@gmail.com>
2024-12-03 10:43:57 -0800

    - Rollback on redundantly using sass in vite.config.js to fix backend

--- packages/react-frontend/vite.config.js
+++ packages/react-frontend/vite.config.js
@@ -1,29 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vitePluginSass from 'vite-plugin-sass';
 import Terminal from "vite-plugin-terminal";
 
 export default defineConfig({
   plugins: [
     react(),
-    vitePluginSass(),
     Terminal({
       console: 'terminal',
       output: ['terminal', 'console']
     })
   ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "./scss/_variables.scss";
-          @import "./scss/_signup.scss";
-          @import "./scss/_login.scss";
-          @import "./scss/_variables.scss";
-          `
-      }
-    }
-  },
   build: {
     rollupOptions: {
       external: ['/@id/__x00__virtual:terminal/console']
