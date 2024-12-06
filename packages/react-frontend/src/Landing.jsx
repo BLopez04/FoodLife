3ced3e78e5ae70c34d5332d51d0b67194489dbbd
ryanvuu <ryanvu077@gmail.com>
2024-12-05 23:13:51 -0800

    Add features section to landing page

--- packages/react-frontend/src/Landing.jsx
+++ packages/react-frontend/src/Landing.jsx
@@ -10,6 +10,7 @@ import food1 from './images/Foo.jpg';
 import food2 from './images/FoodIconTwo.jpg';
 import food3 from './images/FoodIconThree.jpg';
 import food4 from './images/HealthyFoodIcon.png';
+import graphic from './images/Landing-Graphic.png';
 import Navbar from "./Navbar";
 
 
@@ -22,9 +23,28 @@ function Landing() {
                 <div className={classes.landingContent}>
                     <h1 className={classes.bigText}>Welcome to the start of your new <span className={classes.bigFoodLife}>FoodLife</span>.</h1>
                     <p className={classes.descriptionText}>Stay within your personalized limits. Keep track of your personal, EBT, and/or meal plan budgets with the help of FoodLife.</p>
+                    
                     <div className={classes.buttonSignLog}>
                         <button className={classes.loginButton} onClick={() => navigate("/login")}>Login</button>
                         <button className={classes.signButton} onClick={() => navigate("/signup")}>Sign Up</button>
+                    </div>
+                </div>
+            </div>
+
+            <div id="features" className={classes.featuresSection}>
+                <h2 className={classes.featuresTitle}>Features</h2>
+                <div className={classes.featuresContainer}>
+                    <div className={classes.featureItem}>
+                        <h3>Budgeting</h3>
+                        <p>Set and keep track of budgets in personal, meal plan, and grocery budgets.</p>
+                    </div>
+                    <div className={classes.featureItem}>
+                        <h3>Meal Logging</h3>
+                        <p>Easy-to-use table to log meals and their costs.</p>
+                    </div>
+                    <div className={classes.featureItem}>
+                        <h3>Group Expenses</h3>
+                        <p>Personal, EBT, and Grocery categories allow for clear grouping and division of budgeting and meal tracking.</p>
                     </div>
                 </div>
             </div>
